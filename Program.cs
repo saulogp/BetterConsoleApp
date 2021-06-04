a4a5eebb4a264e7af970e3bb2b55886eb8f2a26e
saulogp <sauloproetti@gmail.com>
2021-06-04 11:44:44 -0300

    CreateDefaultBuilder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace BetterConsoleApp
 {
@@ -6,7 +10,33 @@ namespace BetterConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = (ILogger)new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Applications Starting...");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+
+                })
+                .UseSerilog()
+                .Build();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
+
         }
     }
 }
